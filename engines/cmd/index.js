c7606b41f999c6af580b13657fa4ab6d5d8ac553
ruchiraw <ruchira.wageesha@gmail.com>
2014-05-31 16:31:25 +0530

    fixing issues in cmd tool

--- engines/cmd/index.js
+++ engines/cmd/index.js
@@ -2,10 +2,18 @@ var jaggery = global.jaggery;
 var engine = jaggery.get('engine');
 var writer = jaggery.get('writer');
 
+var File = Packages.java.io.File;
+var SEPARATOR = File.separator;
+var prefix = '(function(print, require){';
+var suffix = '})';
+
 var print = function (obj) {
     writer.println(obj);
 };
 
+var requir = global.requirer(new File('').getAbsolutePath() + SEPARATOR + '/index.js', global.resolver);
+
 module.exports = function (options) {
-    return engine.eval(options.get('source'));
+    var fn = engine.eval(prefix + options.get('source') + suffix);
+    fn(print, requir);
 };
